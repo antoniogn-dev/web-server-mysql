28ed93d85fb514412d81cb6c3569d51caec2a883
antoniogn-dev <antoniogn2810@gmail.com>
2026-07-09 11:02:25 -0500

    adicionando cors 09/07/26

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express"
 import empleadosRoutes from "./routes/empleados.routes.js";
 import indexRoutes from "./routes/index.routes.js"
 import { PORT } from "./config/config.js";
+import cors from "cors";
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use(indexRoutes)
